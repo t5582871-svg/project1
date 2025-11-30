56403403968c212e75f6c171b0ce0356478840e3
t5582871-svg <dsvredlov@mail.ru>
2025-11-30 14:24:02 +0300

    Added get_massage method

--- src/main/java/ru/qaguru/Main.java
+++ src/main/java/ru/qaguru/Main.java
@@ -7,7 +7,6 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf(get_massage());
-        System.out.printf("What is your name? %n");
     }
     private static String get_massage() {
         return "Hello QA GURU!!!!";
