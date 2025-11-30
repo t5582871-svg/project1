dbf059111f062c82d6e76e9854fec0f7e41a7602
t5582871-svg <dsvredlov@mail.ru>
2025-11-30 14:15:37 +0300

    Added "What is your name? %n"

--- src/main/java/ru/qaguru/Main.java
+++ src/main/java/ru/qaguru/Main.java
@@ -7,6 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf(get_massage());
+        System.out.printf("What is your name? %n");
     }
     private static String get_massage() {
         return "Hello QA GURU!!!!";
